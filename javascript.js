d6c21005c85edd1f1f6fd657488773fb77353c0d
Bacho13 <38540902+Bacho13@users.noreply.github.com>
2021-12-07 00:11:25 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// საწყისი
+let value = 0;
+
+// ბუთთონებისთვის და მნიშვნელობისთვის ძალის მინიჭება
+const value = document.querySelector("#value");
+const btns = document.querySelector(".btn");
+
+btns.forEach(function(btn)  {
+    
+  btn.addEvenLeasener
+
+
+});
